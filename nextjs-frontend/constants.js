d3ebd0de1230c71a0d12832b9f3e8716b0a6cf44
Kage <romzhey@gmail.com>
2022-12-25 20:08:22 +0800

    added nextjs frontend

---
+++ nextjs-frontend/constants.js
@@ -0,0 +1,3 @@
+export const SUBGRAPH_URL =
+  "https://api.thegraph.com/subgraphs/name/kagerrak/celo-nft-marketplace";
+export const MARKETPLACE_ADDRESS = "0x07cFD8E5D8bAEC70c6Aa0da8D7aEa06F4A98600d";
